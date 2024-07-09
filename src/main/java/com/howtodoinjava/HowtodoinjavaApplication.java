92108e982c96ea15a54f9fe2ffe1326c6c8a3a14
Erick Garcia <eggarcia9814@gmail.com>
2024-07-09 13:55:43 +1000

    spring web service init

---
+++ src/main/java/com/howtodoinjava/HowtodoinjavaApplication.java
@@ -0,0 +1,12 @@
+package com.howtodoinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtodoinjavaApplication {
+	  public static void main(String[] args) {
+		    SpringApplication.run(HowtodoinjavaApplication.class);
+		  }
+
+}
